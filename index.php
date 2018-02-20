1fe9b2b569edc6d2d9326a5149a9b310655d7794
Fernando Maradei <Clienti@MI00326919.transp.biz>
2018-02-20 14:33:24 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        
+        $c = new Carro(2);
+        echo $c->getMotor();
+        
+            
+        class Carro{
+            private $pneus;
+            private $motor;
+            
+            public function __construct($motor) {
+                $this->motor = $motor;
+            }
+            
+            public function getMotor(){
+                return $this->motor;
+            }
+            
+        }
+        
+        ?>
+    </body>
+</html>
